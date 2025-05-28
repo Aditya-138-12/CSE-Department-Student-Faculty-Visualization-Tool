466de54fea231ac602b59056ca985678d110e88d
Aditya-138-12 <adityasaroha456@gmail.com>
2025-05-28 10:59:37 +0530

    Added some more contextual related data to the platform. 1. Mostly added the `under constrution` and `coming soon`.

--- src/components/NammaCompiler/NammaCompilerEditor/nammaCompilerInput.js
+++ src/components/NammaCompiler/NammaCompilerEditor/nammaCompilerInput.js
@@ -4,7 +4,7 @@ const NammaCompilerInput = ({ isDark }) => {
     return (
         <>
             <div className={`nammaCompilerInputDiv ${isDark ? 'dark-theme' : 'light-theme'}`}>
-                <textarea type='text' style={{ fontSize: "0.8rem", resize: "none" }} placeholder='Program Input' className={`nammaCompilerInput ${isDark ? 'dark-theme' : 'light-theme'}`} />
+                <textarea type='text' style={{ fontSize: "0.8rem", resize: "none" }} placeholder='Program Input (Under Constrution, will be available soon!!)' className={`nammaCompilerInput ${isDark ? 'dark-theme' : 'light-theme'}`} />
             </div >
         </>
     );
--- src/components/NammaCompiler/NammaCompilerEnterTitle/nammaCompilerEnterTitle.js
+++ src/components/NammaCompiler/NammaCompilerEnterTitle/nammaCompilerEnterTitle.js
@@ -5,7 +5,7 @@ const NammaCompilerEnterTitle = ({ isDark }) => {
     return (
         <>
             <div className={`${isDark ? 'dark-theme' : 'light-theme'} nammaCompilerEnterTitle`}>
-                <input type='text' placeholder='Enter Title' className={`${isDark ? 'dark-theme' : 'light-theme'} nammaCompilerEnterTitleInput`} />
+                <input type='text' placeholder='Enter Title (will be available when the code share and save will be functional)' className={`${isDark ? 'dark-theme' : 'light-theme'} nammaCompilerEnterTitleInput`} />
             </div >
         </>
     );
